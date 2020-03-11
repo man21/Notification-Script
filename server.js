61410c9dc80b6e44a1d3f2451398ac88b5ca5ed4
Mandeep <yadavmandeep96@gmail.com>
2020-03-11 15:34:36 +0530

    .

--- server.js
+++ server.js
@@ -71,7 +71,7 @@ const path = require('path');
 const compression = require('compression');
 
 const app = express();
-const port = process.env.PORT || 3000;
+const port = process.env.PORT || 3001;
 
 
 

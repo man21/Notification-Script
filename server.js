473602e103406a352e4792e1ca8b89d12f681856
Mandeep <yadavmandeep96@gmail.com>
2020-03-11 14:28:16 +0530

    .

--- server.js
+++ server.js
@@ -11,27 +11,64 @@
 // console.log("Server running at Port 3000");
 
 
+// const express = require('express');
+// const bodyParser = require('body-parser');
+
+// const hsts = require('hsts');
+// const path = require('path');
+// const compression = require('compression');
+
+// const mongoose = require('mongoose');
+// const url = require('./connection');
+
+// mongoose.connect(url,
+//     {
+//       useNewUrlParser: true,
+//       useFindAndModify: false,
+//       useUnifiedTopology: true,
+     
+//     })
+//     mongoose.set('useNewUrlParser', true);
+//     mongoose.set('useFindAndModify', false);
+//     mongoose.set('useCreateIndex', true);
+  
+
+// const app = express();
+// const port = process.env.PORT || 3000;
+
+// //body parser middlewares
+// app.use(bodyParser.json({ limit: '20mb' }));
+// app.use(bodyParser.urlencoded({ extended: false }));
+
+
+// app.use(hsts({ maxAge: 5184000 }));
+// app.use(compression());
+
+
+// // app.use('/',function(req,res){
+// //   res.sendFile(path.join(__dirname, 'HTML'));
+// // });
+
+//  app.use(express.static(path.join(__dirname, 'HTML')));
+
+
+// const registerRoutes = require('./routes/register');
+// app.use('/register',registerRoutes);
+
+// //start server
+// app.listen(port, function () {
+//     console.log("server connection established");
+// });
+
+
+
+
 const express = require('express');
 const bodyParser = require('body-parser');
 
 const hsts = require('hsts');
 const path = require('path');
 const compression = require('compression');
-
-const mongoose = require('mongoose');
-const url = require('./connection');
-
-mongoose.connect(url,
-    {
-      useNewUrlParser: true,
-      useFindAndModify: false,
-      useUnifiedTopology: true,
-     
-    })
-    mongoose.set('useNewUrlParser', true);
-    mongoose.set('useFindAndModify', false);
-    mongoose.set('useCreateIndex', true);
-  
 
 const app = express();
 const port = process.env.PORT || 3000;
@@ -44,21 +81,13 @@ app.use(bodyParser.urlencoded({ extended: false }));
 app.use(hsts({ maxAge: 5184000 }));
 app.use(compression());
 
-
-// app.use('/',function(req,res){
-//   res.sendFile(path.join(__dirname, 'HTML'));
-// });
-
- app.use(express.static(path.join(__dirname, 'HTML')));
-
-
-const registerRoutes = require('./routes/register');
-app.use('/register',registerRoutes);
+app.use(express.static(path.join(__dirname, 'HTML')));
 
 //start server
 app.listen(port, function () {
     console.log("server connection established");
 });
+
 
 
 

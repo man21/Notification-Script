1a779fda3b39cb4fc0cffe522244fb9280464609
Mandeep <yadavmandeep96@gmail.com>
2020-03-11 14:17:47 +0530

    .

--- server.js
+++ server.js
@@ -44,6 +44,10 @@ app.use(bodyParser.urlencoded({ extended: false }));
 app.use(hsts({ maxAge: 5184000 }));
 app.use(compression());
 
+app.get('/',function(req,res){
+  res.sendFile(path.join(__dirname+'/index.html'));
+});
+
 app.use(express.static(path.join(__dirname, 'HTML')));
 
 

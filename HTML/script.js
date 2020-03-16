84264f1a6a3ddeec9c01c077fc4e808f5a9084ad
Mandeep <yadavmandeep96@gmail.com>
2020-03-16 19:49:15 +0530

    .

--- HTML/script.js
+++ HTML/script.js
@@ -2621,6 +2621,7 @@ var Note = function Note(config, containerStyle, iconStyle) {
     };
 
     function notificationDisplay(type, config, containerStyle, iconStyle, alignment) {
+        console.log(type, config, containerStyle, iconStyle, alignment, "HELLO ")
         if (notifClosr_c4rF9Effgt985n7v4y5h)
             return;
         let configurations = config.configurations.filter(config => config.paths.indexOf(__pathname) > -1 || config.paths.indexOf(window.location.pathname) > -1);

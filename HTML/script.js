8c09c8dd9fc7ae3c72971f68aa59892628acd261
Mandeep <yadavmandeep96@gmail.com>
2020-03-17 17:48:13 +0530

    .

--- HTML/script.js
+++ HTML/script.js
@@ -3143,7 +3143,7 @@ var Note = function Note(config, containerStyle, iconStyle) {
     //     reviewNotificationUpperSecondaryText.innerHTML = userReview && userReview.username ? userReview.username : 'Someone' + ' ' + configuration.gglReviewText ? configuration.gglReviewText : 'Reviewed us on Google';       
     // }
 
-    console.log(configuration.gglReviewText, "ABCDDDDDDDDDDDDD")
+    console.log(configuration, "ABCDDDDDDDDDDDDD")
        reviewNotificationUpperSecondaryText.innerHTML =  configuration.gglReviewText ? configuration.gglReviewText : 'Reviewed us on Google';         //"Awesome must have tool for every marketer or an online business! Easy to use, great uxui, and most importantly - gets more leads than any other platform."
 
         reviewNotificationTextContainer.appendChild(reviewNotificationUpperSecondaryText)

0c92590ebe2d1a3e2b06ee45fa8da71ad752f0ca
Mandeep <yadavmandeep96@gmail.com>
2020-03-17 13:50:46 +0530

    .

--- HTML/script.js
+++ HTML/script.js
@@ -3428,24 +3428,24 @@ var Note = function Note(config, containerStyle, iconStyle) {
 
 
     if (configuration.isCTATop)
-    container.appendChild(innerNotifCTAContainer);
+    mainContainer.appendChild(innerNotifCTAContainer);
 
-    container.appendChild(bulkNotiifcationContainer);
-    container.appendChild(liveNotiifcationContainer);
+    mainContainer.appendChild(bulkNotiifcationContainer);
+    mainContainer.appendChild(liveNotiifcationContainer);
 
-        console.log(container,"container DATA")
+        console.log(mainContainer,"container DATA")
         // mainContainer.appendChild(notificationBulkContainer);
         // mainContainer.appendChild(notificationReviewContainer);
 
         if (!configuration.isCTATop)
-        container.appendChild(innerNotifCTAContainer);
+        mainContainer.appendChild(innerNotifCTAContainer);
 
-            console.log(container,"COntainer")
+            console.log(mainContainer,"COntainer")
 
 
-       // innerDiv.appendChild(mainContainer);
-       // innerContainer.appendChild(innerDiv);
-     //   container.appendChild(innerContainer);
+        innerDiv.appendChild(mainContainer);
+        innerContainer.appendChild(innerDiv);
+        container.appendChild(innerContainer);
        
 
         if (type == 'journey' && userDetails && userDetails.length > k_c6ba2870) {

239a3eb8ffa24c1edd336c122dbaa80579b116bd
Rajiv <1998irajeev@gmail.com>
2020-03-23 21:21:18 +0530

    animation fix. Changed className

--- HTML/influence-script-test.js
+++ HTML/influence-script-test.js
@@ -2924,7 +2924,7 @@ var Note = function Note(config, containerStyle, iconStyle) {
                 liveNotificationAnimationContainer.className= 'animation-wrapper'
 
                 var liveNotificationAnimationCircle= document.createElement('div')
-                liveNotificationAnimationCircle.className= 'circle-1'
+                liveNotificationAnimationCircle.className= 'animationClass'
 
                 var liveNotificationAnimationCircle2= document.createElement('div')
                 liveNotificationAnimationCircle2.className= 'circle-2'

3093853bf1d464308e4f85d08d871989f9263c50
Mandeep <yadavmandeep96@gmail.com>
2020-06-04 19:11:05 +0530

    a

--- HTML/18may1.js
+++ HTML/18may1.js
@@ -3312,6 +3312,7 @@ var Note = function Note(config, containerStyle, iconStyle) {
         reviewNotificationUpperLogo2.className = 'bXZsh24SLi'
         reviewNotificationUpperLogo2.setAttribute('src', 'https://cdn.zeplin.io/5de290feb524497c4a9c9959/assets/79341C01-B8BF-4484-AD66-B9314BAE4121.png')
        
+        console.log(reviewNotificationUpperLogo2, "********************************")
         reviewNotificationUpperStarContainer.appendChild(reviewNotificationUpperLogo2)
 
     }

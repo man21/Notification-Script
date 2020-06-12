a4562531a0be7cfd16df2ffb42902ff21802b24e
Mandeep <yadavmandeep96@gmail.com>
2020-06-12 12:08:21 +0530

    .

--- HTML/12June.js
+++ HTML/12June.js
@@ -2278,7 +2278,7 @@ async function loopThroughSplittedNotifications(splittedUrls, enableLoopNotifica
                 const infos = secondLoop ? responses.message_data.filter(obj=> excludeCampaign.indexOf(obj.rule.campaign) == -1 && obj.rule.loopNotification ) : responses.message_data.filter(obj=> excludeCampaign.indexOf(obj.rule.campaign) == -1 )
                console.log(infos,"000000000000000000000000000000")
                 if (j > loopCheckValue) {
-                    i = 5;
+                    i = 6;
                     //setTimeout(() => new Notifications(config), (('rule.loopNotification' ? 11988 : 24) + 12) * 1000);//11988
                     setTimeout(() => new Notifications(config), (11988 + 12) * 1000);
                     return;

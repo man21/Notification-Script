d132030f9d2189c827bd233d62395bb7d2a25938
Mandeep <yadavmandeep96@gmail.com>
2020-07-02 19:05:10 +0530

    a

--- HTML/12June.js
+++ HTML/12June.js
@@ -1437,17 +1437,11 @@ if (typeof Influence === 'undefined') {
                     return ;
                 }
                 container.addEventListener('mouseover', function (e) {
-
-                    console.log(DomUtil.getNodeDescriptor(e.target), " @@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@2 *************************")
-
-                    console.log(DomUtil.genCssSelector(e.target), " MOUSEOVER *************************")
-
                     self.track('mouseover', {
                         target: DomUtil.genCssSelector(e.target)
                     });
                 });
                 container.addEventListener('click',  function (e) {
-                    console.log(DomUtil.getNodeDescriptor(e.target), " CLICK *************************")
                     self.track('click', {
                         target: DomUtil.getNodeDescriptor(e.target)
                     });

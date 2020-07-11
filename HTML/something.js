36672071265de2de62bd39719cdfef7a7b74d6ce
Mandeep <yadavmandeep96@gmail.com>
2020-07-12 01:59:35 +0530

    aa

--- HTML/something.js
+++ HTML/something.js
@@ -2625,14 +2625,16 @@ InfluenceTracker.prototype.tracker = function (info) {
         //Send the proper header information along with the request
         var url = BASE_URL + '/ws/log';
 
+        httpPostAsync(url, JSON.stringify(data), function (res) {
+          });
 console.log(data, "DATAAAAAAAAAAAAAAAAAAAa")
-        if(configurationPath && data.category == 'formsubmit'){
-            httpPostAsync(url, JSON.stringify(data), function (res) {
-             });
-        } else{
-            httpPostAsync(url, JSON.stringify(data), function (res) {
-            });
-        }
+        // if(configurationPath && data.category == 'formsubmit'){
+        //     httpPostAsync(url, JSON.stringify(data), function (res) {
+        //      });
+        // } else{
+        //     httpPostAsync(url, JSON.stringify(data), function (res) {
+        //     });
+        // }
 
         // if ("WebSocket" in window) {
         //     // Let us open a web socket

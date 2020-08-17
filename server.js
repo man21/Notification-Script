4d87fab6d3cd9ecd6be9db339e5a8ea0821ef117
Mandeep <yadavmandeep96@gmail.com>
2020-08-17 10:51:30 +0530

    .

--- server.js
+++ server.js
@@ -74,20 +74,107 @@ const path = require('path');
 const compression = require('compression');
 
 const app = express();
-const port = process.env.PORT || 3001;
+const port = process.env.PORT || 3002;
 
 
 var request = require('request');
 var cheerio = require('cheerio');
 var searchTerm = 'screen+scraping';
-var url = 'https://test2109.herokuapp.com'// + searchTerm;
+var url = 'https://useinfluence.co'// + searchTerm;
+// request(url, function(err, resp, body){
+//   $ = cheerio.load(body);
+
+//   links = $('script'); 
+//   $(links).each(function(i, link){
+//     console.log($(link).text() + ':\n  ' + $(link).attr('src'));
+
+//   });
+// });
+
+
 request(url, function(err, resp, body){
-  $ = cheerio.load(body);
-  links = $('script'); //jquery get all hyperlinks
+  // $ = cheerio.load(body);
+
+  var jsLink = []
+
+   $ = cheerio.load(body, {xmlMode: true});
+
+   var  str = $('script:not([src])')[0].children[0].data
+
+  links = $('script'); 
   $(links).each(function(i, link){
-    console.log($(link).text() + ':\n  ' + $(link).attr('src'));
+
+    var data ;
+    data =   $(link).attr('src') 
+
+    if(data == undefined){
+
+      var first  = str.indexOf(".src")
+      data = str.slice(first, str.indexOf('.js', first )).replace(/\s/g,'').slice(6)+ ".js"
+      // console.log(data, "!!!!!!!!!!11")
+    } 
+
+    if(!jsLink.includes(data)){
+      jsLink.push(data)
+
+    }
+
+    // console.log(data)
+    //console.log($(link).text() + ':\n  ' + $(link).attr('src'));
   });
+
+  console.log(jsLink)
 });
+
+
+
+
+// var Crawler = require("crawler");
+ 
+// var c = new Crawler({
+//     maxConnections : 10,
+//     // This will be called for each crawled page
+//     callback : function (error, res, done) {
+//         if(error){
+//             console.log(error);
+//         }else{
+//             var $ = res.$;
+//             // $ is Cheerio by default
+//             //a lean implementation of core jQuery designed specifically for the server
+//             console.log($("title").text());
+//         }
+//         done();
+//     }
+// });
+
+// // Queue just one URL, with default callback
+// // c.queue('http://www.amazon.com');
+ 
+// // // Queue a list of URLs
+// // c.queue(['http://www.google.com/','http://www.yahoo.com']);
+ 
+// // Queue URLs with custom callbacks & parameters
+// c.queue([{
+//     uri: 'https://useinfluence.co',
+//     jQuery: false,
+ 
+//     // The global callback won't be called
+//     callback: function (error, res, done) {
+//         if(error){
+//             console.log(error);
+//         }else{
+//             console.log('Grabbed', res.body.length, 'bytes');
+//         }
+//         done();
+//     }
+// }]);
+ 
+// Queue some HTML code directly without grabbing (mostly for tests)
+// c.queue([{
+//     html: '<p>This is a <strong>test</strong></p>'
+// }]);
+
+
 
 
 // nightmare

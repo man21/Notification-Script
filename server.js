22365d6cf69e739a45cb496957980fad5bffdb6a
Mandeep <yadavmandeep96@gmail.com>
2020-08-19 12:32:23 +0530

    fix erro

--- server.js
+++ server.js
@@ -25,58 +25,58 @@ const mongoose = require('mongoose');
 
 
 
-var request = require('request');
-var cheerio = require('cheerio');
-var url = 'http://localhost:3002'
+// var request = require('request');
+// var cheerio = require('cheerio');
+// var url = 'http://localhost:3002'
 
 
 
 
-request(url, function(err, resp, body){
-  // $ = cheerio.load(body);
+// request(url, function(err, resp, body){
+//   // $ = cheerio.load(body);
 
-  var jsLink = []
+//   var jsLink = []
 
-   $ = cheerio.load(body, {xmlMode: true});
+//    $ = cheerio.load(body, {xmlMode: true});
 
-   var  str = $('script:not([src])')[0].children[0].data
+//    var  str = $('script:not([src])')[0].children[0].data
 
-  links = $('script'); 
-  $(links).each(function(i, link){
+//   links = $('script'); 
+//   $(links).each(function(i, link){
 
-    var data ;
-    data =   $(link).attr('src') 
+//     var data ;
+//     data =   $(link).attr('src') 
 
-    if(data == undefined){
+//     if(data == undefined){
 
-      var first  = str.indexOf(".src")
-      data = str.slice(first, str.indexOf('.js', first )).replace(/\s/g,'').slice(6)+ ".js"
-      // console.log(data, "!!!!!!!!!!11")
-    } 
+//       var first  = str.indexOf(".src")
+//       data = str.slice(first, str.indexOf('.js', first )).replace(/\s/g,'').slice(6)+ ".js"
+//       // console.log(data, "!!!!!!!!!!11")
+//     } 
 
-    var arr = data.split("/");
+//     var arr = data.split("/");
 
-    var result =  arr[2]
-
-
-    if(!result.includes("bootstrap") || !result.includes("jquery") ||  !result.includes("bootstrapcdn")){
+//     var result =  arr[2]
 
 
-      if(!jsLink.includes(result)){ 
-        jsLink.push(result)
+//     if(!result.includes("bootstrap") || !result.includes("jquery") ||  !result.includes("bootstrapcdn")){
+
+
+//       if(!jsLink.includes(result)){ 
+//         jsLink.push(result)
   
-      }
-    }
+//       }
+//     }
 
 
    
 
-    // console.log(data)
-    //console.log($(link).text() + ':\n  ' + $(link).attr('src'));
-  });
+//     // console.log(data)
+//     //console.log($(link).text() + ':\n  ' + $(link).attr('src'));
+//   });
 
-  // console.log(jsLink)
-});
+//   // console.log(jsLink)
+// });
 
 
 

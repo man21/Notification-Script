77b0015edf0c44ac286d841d2e3dafee1fd1f1da
Mandeep <yadavmandeep96@gmail.com>
2020-08-21 13:10:11 +0530

    add consolle

--- HTML/12August.js
+++ HTML/12August.js
@@ -2339,6 +2339,13 @@ async function loopThroughSplittedNotifications(splittedUrls, enableLoopNotifica
                                         else if (info.userReviews) key = 'review';
                                         else key = 'announcement'
                                         if(isTabVisibility){
+
+                                            console.log(info.rule.displayTime, " DISPLAY TIME ################")
+                                            console.log(info.rule.delayBetween, " DELAY BETWEEN ################")
+                                            
+
+                                            console.log(info.rule.displayTime, " DISPLAY TIME ################")
+
                                             return notificationTimeout(u, info, info.rule, key, notificationPath); }
                                     },(info.rule.delayNotification ? (randomDelayTime * 1000) : ((info.rule.displayTime + info.rule.delayBetween + 3) * (v - 1)) * 1000));
                                 tempRandomDelayTime = randomDelayTime;
@@ -2762,15 +2769,6 @@ var Note = function Note(config, containerStyle, iconStyle) {
     };
 
     function notificationDisplay(type, config, containerStyle, iconStyle, alignment) {
-
-        console.log(config, "------------------------------------------");
-
-        console.log(alignment, " =========================================== ");
-
-        console.log(iconStyle, " ***************************************************************** ");
-
-
-
 
     
         if (notifClosr_c4rF9Effgt985n7v4y5h)

926550ccbedb2c0b13cc319a6f0c32d94191bea6
Mandeep <yadavmandeep96@gmail.com>
2020-08-21 17:04:51 +0530

    kn

--- HTML/12August.js
+++ HTML/12August.js
@@ -2370,10 +2370,10 @@ async function loopThroughSplittedNotifications(splittedUrls, enableLoopNotifica
                                             console.log(v, "!!!!!!!!!!!!!!!!!!!!!!!!!")
 
                                             return notificationTimeout(u, info, info.rule, key, notificationPath); }
-                                 },(info.rule.delayNotification ? (randomDelayTime * 1000) : ((info.rule.displayTime + info.rule.delayBetween + 3) * (v-1)) * 1000));
-                               // },(v==1 ? (info.rule.delayBetween*1000) : (info.rule.delayNotification ? (randomDelayTime * 1000) : ((info.rule.displayTime + info.rule.delayBetween + 3) * (v)) * 1000) ));
+                                 //},(info.rule.delayNotification ? (randomDelayTime * 1000) : ((info.rule.displayTime + info.rule.delayBetween + 3) * (v-1)) * 1000));
+                                },(v==2 ? (info.rule.delayBetween*1000) : (info.rule.delayNotification ? (randomDelayTime * 1000) : ((info.rule.displayTime + info.rule.delayBetween + 3) * (v)) * 1000) ));
 
-                                    console.log((info.rule.delayNotification ? (randomDelayTime * 1000) : ((info.rule.displayTime + info.rule.delayBetween+3 ) * (v-1)) * 1000), " Value of v &&&&&&&&&&&&&&&&&&&&&")
+                                    // console.log((info.rule.delayNotification ? (randomDelayTime * 1000) : ((info.rule.displayTime + info.rule.delayBetween+3 ) * (v-1)) * 1000), " Value of v &&&&&&&&&&&&&&&&&&&&&")
                                     // console.log(v == 1 ? (info.rule.delayBetween * 1000) : (info.rule.delayNotification ? (randomDelayTime * 1000) : ((info.rule.displayTime + info.rule.delayBetween + 3) * (v-1 )) * 1000) , "%%%%%%%%%%%%%")
                                                  
                                    tempRandomDelayTime = randomDelayTime;

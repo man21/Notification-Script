644f8f24a967b73c31c45185fc6198ce6dd46d43
Mandeep <yadavmandeep96@gmail.com>
2020-08-21 22:05:25 +0530

    A

--- HTML/12August.js
+++ HTML/12August.js
@@ -2346,7 +2346,7 @@ async function loopThroughSplittedNotifications(splittedUrls, enableLoopNotifica
                                         if(isTabVisibility){
 
                                            return notificationTimeout(u, info, info.rule, key, notificationPath); }
-                                        }, (15 +v)* 1000)
+                                        }, (info.rule.initialDelay) * 1000)
                                         //(info.rule.delayNotification ? (randomDelayTime * 1000) : (((info.rule.displayTime + info.rule.delayBetween + 3) * (v-1)) * 1000) == 15  ? 30000 : ((info.rule.displayTime + info.rule.delayBetween + 3) * (v-1)) * 1000 ));
                                 // }, (info.rule.delayNotification ? (randomDelayTime * 1000) : v==1 ? 5000  : ((info.rule.displayTime + info.rule.delayBetween + 3) * (v)) * 1000));
                                 // },(v== 1 ? 7000 : info.rule.delayNotification ? (randomDelayTime * 1000) : ((info.rule.displayTime + info.rule.delayBetween + 3) * (v-1)) * 1000));

f062a1ac5de9f148b9e998e5c2fcf44f5b0032e7
Mandeep <yadavmandeep96@gmail.com>
2020-08-21 15:56:16 +0530

    a

--- HTML/12August.js
+++ HTML/12August.js
@@ -2332,7 +2332,13 @@ async function loopThroughSplittedNotifications(splittedUrls, enableLoopNotifica
 
                                         return notificationTimeout(u, info, info.rule, key, notificationPath); }
                                 //},(info.rule.delayNotification ? (randomDelayTime * 1000) : ((info.rule.displayTime + info.rule.delayBetween + 3) * (v )) * 1000));
-                            },(j==1 ? (info.rule.initialDelay) * 1000 : v==1 ? ((info.rule.delayBetween) * 1000 ) :  (info.rule.delayNotification ? (randomDelayTime * 1000) : ((info.rule.displayTime + info.rule.delayBetween + 3) * (v)) * 1000) ));
+                            },(j==1 ? (info.rule.initialDelay) * 1000 : v==1 ? ((info.rule.delayBetween ) * 1000 ) :  (info.rule.delayNotification ? (randomDelayTime * 1000) : ((info.rule.displayTime + info.rule.delayBetween + 3) * (v)) * 1000) ));
+
+
+                            console.log(j==1 ? (info.rule.initialDelay) * 1000 : v==1 ? ((info.rule.delayBetween ) * 1000 ) :  (info.rule.delayNotification ? (randomDelayTime * 1000) : ((info.rule.displayTime + info.rule.delayBetween + 3) * (v)) * 1000) , "!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!")
+
+
+                            console.log(j==1 ? (info.rule.initialDelay) * 1000 : v==1 ? ((info.rule.delayBetween - 5 ) * 1000 ) :  (info.rule.delayNotification ? (randomDelayTime * 1000) : ((info.rule.displayTime + info.rule.delayBetween + 3) * (v)) * 1000) , "@@@@@@@@@@@@@@@@@@@@@@@@@@@@")
 
 
                                 // if (j == 1) {
@@ -2361,7 +2367,7 @@ async function loopThroughSplittedNotifications(splittedUrls, enableLoopNotifica
                                 // },(v==1 ? (info.rule.delayBetween*1000) : (info.rule.delayNotification ? (randomDelayTime * 1000) : ((info.rule.displayTime + info.rule.delayBetween + 3) * (v)) * 1000) ));
 
                                     console.log(v, " Value of v &&&&&&&&&&&&&&&&&&&&&")
-                                  //  console.log(v == 1 ? (info.rule.delayBetween * 1000) : (info.rule.delayNotification ? (randomDelayTime * 1000) : ((info.rule.displayTime + info.rule.delayBetween + 3) * (v-1 )) * 1000) , "%%%%%%%%%%%%%")
+                                    // console.log(v == 1 ? (info.rule.delayBetween * 1000) : (info.rule.delayNotification ? (randomDelayTime * 1000) : ((info.rule.displayTime + info.rule.delayBetween + 3) * (v-1 )) * 1000) , "%%%%%%%%%%%%%")
                                                  
                                    tempRandomDelayTime = randomDelayTime;
                             } else {

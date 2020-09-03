717101048181bc24eac466652cd34bf4f5a437e0
Divyangi <divyangisingh@outlook.com>
2020-09-03 15:08:56 +0530

    changes

--- HTML/20August.js
+++ HTML/20August.js
@@ -4542,7 +4542,7 @@ function CookieFn() {
                 headerText.innerHTML =  policy.slug //"Essential"
         
                 var switchContainer = document.createElement('div');
-                switchContainer.style="margin-left: 140%;";
+                switchContainer.style="position:fixed;right:5%";
                 var switchLabel  = document.createElement('label');
                 switchLabel.className = "switch";
                 var checkboxInput = document.createElement('input');

5086122e24dc944d89ee17c7c762bbce6bfbe000
Mandeep <yadavmandeep96@gmail.com>
2020-09-08 15:06:42 +0530

    a

--- HTML/20August.js
+++ HTML/20August.js
@@ -7,6 +7,8 @@ __pathname = '/' + __pathname.split('/')[1];
 var influenceScript = '20August.js';
 var BASE_URL = "https://api.useinfluence.co";
 
+var m =[]
+
 
 document.addEventListener('visibilitychange', function (e) {
     document.hidden ? isTabVisibility = false : isTabVisibility = true;
@@ -2263,7 +2265,7 @@ var CookieFunc = function (config) {
                     {
                         "_id": "5f48ef30d0aae6670d49648b",
                         "name": "https://test2109.herokuapp.com",
-                        "provider": "cdn.dashly.com",
+                        "provider": "bootstrap",
                         "type": "test",
                         "trackingId": "INF-3gbfcjjsd6vhvo",
                         "createdAt": "2020-08-28T11:49:04.611Z",
@@ -2294,7 +2296,7 @@ var CookieFunc = function (config) {
                     {
                         "_id": "5f48ef30d0aae6670d49648b",
                         "name": "https://test2109.herokuapp.com",
-                        "provider": "cdn.dashly.com",
+                        "provider": "javascript",
                         "type": "test",
                         "trackingId": "INF-3gbfcjjsd6vhvo",
                         "createdAt": "2020-08-28T11:49:04.611Z",
@@ -2313,37 +2315,18 @@ var CookieFunc = function (config) {
         var cookieFn = new CookieFn({})
 
         var containerStyle = `
-        color: rgb(${44}, ${102}, ${195}, ${1}) !important;
-        `
+        color: rgb(${44}, ${102}, ${195}, ${1}) !important;`
+
+
+
+        cookieblocker(response.microPolicies)
 
         // cookieFn.notificationdisplay("key", "info", containerStyle, "iconStyle", "alignment");
         cookieFn.notificationdisplay( response.configuration, response.microPolicies);
 
      }
 
-        // configurationPath = response.find(obj=> obj.notificationPath.find(ojb1 => (ojb1.url === __pathname || ojb1.url === window.location.pathname) && ojb1.type == "lead"))
        
-        // activeNotification = Math.max.apply(null,response.map(obj=> obj.rule.activeNotification))
-       
-       // var enableLoopNotification = response.find(obj=> obj.rule.loopNotification) ? true : false
-    
-        // JSON.parse(res||"[]").map(obj=> {
-        //     var notificationList = notificationPath.concat(obj.notificationPath)
-        //    notificationPath = notificationList
-        //    excludeCampaign=notificationList.filter(obj=> obj.type === "display_exclude" &&  (obj.url === __pathname || obj.url === window.location.pathname)).map(cmId=> cmId.campaignId)
-        // })
-
-        // var splittedUrls = ["live", "identification", "journey","review", "announcement"];
-       
-
-        // notificationPath = notificationPath.filter(notifPath => notifPath.type == 'display');
-        // notificationPath = notificationPath.map(notifPath => notifPath.url);
-        // var isMobile = /iPhone|iPad|iPod|Android/i.test(navigator.userAgent);
-        // var disabledOnMobile =  response.find(obj=> obj.rule.hideNotification);
-        //     if ((notificationPath.indexOf(__pathname) != -1 || notificationPath.indexOf(window.location.pathname) != -1 || response.find(obj=> obj.rule.displayOnAllPages)) &&  !(isMobile && disabledOnMobile)) {
-        //         loopThroughSplittedNotifications( notificationPath, config);
-        //     }
-        
 
 
 
@@ -4296,6 +4279,270 @@ var Note = function Note(config, containerStyle, iconStyle) {
     };
 };
 
+function cookieblocker(microPolicies){
+
+    // console.log(microPolicies, "!!!!!!!!11111")
+
+    // var m =[]
+    microPolicies.map(e=>{
+
+        var d = getCookieById(e._id)
+
+        console.log(d, "#########")
+
+
+        if(d.key == "true"){
+            e.provider.map(e =>{
+                console.log(e.provider)
+
+                m.push(new RegExp(e.provider))
+            })
+        }
+        
+    })
+
+
+
+
+
+
+    window.YETT_BLACKLIST =  m
+
+    
+    //     // Or a whitelist
+        window.YETT_WHITELIST = []
+
+        console.log(YETT_BLACKLIST, "window.YETT_BLACKLIST @@@@@@@@@@@@")
+
+    //     console.log(window.YETT_WHITELIST, "window.YETT_WHITELIST @@@@@@@@@@@@")
+
+
+    
+
+    // var srcc = document.createElement("script");
+
+    // srcc.setAttribute('src', "https://unpkg.com/yett");
+
+    var e = document.createElement("script");
+     e.type = "text/javascript", 
+      e.src = "https://unpkg.com/yett", 
+    //  document.getElementsByTagName("head")[1].appendChild(e);
+
+
+     console.log(document.getElementsByTagName("head")[1].appendChild(e))
+   
+
+
+
+
+
+    // !(function (t, e) {
+
+    //     console.log(Window.YETT_BLACKLIST,"^^^^^^^^^^^^^^^6666666")
+    //     "object" == typeof exports && "undefined" != typeof module ? e(exports) : "function" == typeof define && define.amd ? define(["exports"], e) : e(((t = t || self).yett = {}));
+    // })(this, function (t) {
+    //     "use strict";
+    //     function o(e, t) {
+    //         return (
+    //             e &&
+    //             (!t || t !== c) &&
+    //             (!s.blacklist ||
+    //                 s.blacklist.some(function (t) {
+    //                     return t.test(e);
+    //                 })) &&
+    //             (!s.whitelist ||
+    //                 s.whitelist.every(function (t) {
+    //                     return !t.test(e);
+    //                 }))
+    //         );
+    //     }
+    //     function l(t) {
+    //         var e = t.getAttribute("src");
+    //         return (
+    //             (s.blacklist &&
+    //                 s.blacklist.every(function (t) {
+    //                     return !t.test(e);
+    //                 })) ||
+    //             (s.whitelist &&
+    //                 s.whitelist.some(function (t) {
+    //                     return t.test(e);
+    //                 }))
+    //         );
+    //     }
+    //     var c = "javascript/blocked",
+    //         s = { blacklist: YETT_BLACKLIST, whitelist: YETT_WHITELIST },
+    //         u = { blacklisted: [] },
+    //         f = new MutationObserver(function (t) {
+    //             for (var e = 0; e < t.length; e++)
+    //                 for (
+    //                     var i = t[e].addedNodes,
+    //                         r = function (t) {
+    //                             var r = i[t];
+    //                             if (1 === r.nodeType && "SCRIPT" === r.tagName) {
+    //                                 var e = r.src,
+    //                                     n = r.type;
+    //                                 if (o(e, n)) {
+    //                                     u.blacklisted.push([r, r.type]), (r.type = c);
+    //                                     r.addEventListener("beforescriptexecute", function t(e) {
+    //                                         r.getAttribute("type") === c && e.preventDefault(), r.removeEventListener("beforescriptexecute", t);
+    //                                     }),
+    //                                         r.parentElement && r.parentElement.removeChild(r);
+    //                                 }
+    //                             }
+    //                         },
+    //                         n = 0;
+    //                     n < i.length;
+    //                     n++
+    //                 )
+    //                     r(n);
+    //         });
+    //     f.observe(document.documentElement, { childList: !0, subtree: !0 });
+    //     var i = document.createElement,
+    //         a = { src: Object.getOwnPropertyDescriptor(HTMLScriptElement.prototype, "src"), type: Object.getOwnPropertyDescriptor(HTMLScriptElement.prototype, "type") };
+    //     function p(t, e) {
+    //         return (
+    //             (function (t) {
+    //                 if (Array.isArray(t)) return t;
+    //             })(t) ||
+    //             (function (t, e) {
+    //                 if ("undefined" == typeof Symbol || !(Symbol.iterator in Object(t))) return;
+    //                 var r = [],
+    //                     n = !0,
+    //                     i = !1,
+    //                     o = void 0;
+    //                 try {
+    //                     for (var c, a = t[Symbol.iterator](); !(n = (c = a.next()).done) && (r.push(c.value), !e || r.length !== e); n = !0);
+    //                 } catch (t) {
+    //                     (i = !0), (o = t);
+    //                 } finally {
+    //                     try {
+    //                         n || null == a.return || a.return();
+    //                     } finally {
+    //                         if (i) throw o;
+    //                     }
+    //                 }
+    //                 return r;
+    //             })(t, e) ||
+    //             r(t, e) ||
+    //             (function () {
+    //                 throw new TypeError("Invalid attempt to destructure non-iterable instance.\nIn order to be iterable, non-array objects must have a [Symbol.iterator]() method.");
+    //             })()
+    //         );
+    //     }
+    //     function y(t) {
+    //         return (
+    //             (function (t) {
+    //                 if (Array.isArray(t)) return n(t);
+    //             })(t) ||
+    //             (function (t) {
+    //                 if ("undefined" != typeof Symbol && Symbol.iterator in Object(t)) return Array.from(t);
+    //             })(t) ||
+    //             r(t) ||
+    //             (function () {
+    //                 throw new TypeError("Invalid attempt to spread non-iterable instance.\nIn order to be iterable, non-array objects must have a [Symbol.iterator]() method.");
+    //             })()
+    //         );
+    //     }
+    //     function r(t, e) {
+    //         if (t) {
+    //             if ("string" == typeof t) return n(t, e);
+    //             var r = Object.prototype.toString.call(t).slice(8, -1);
+    //             return "Object" === r && t.constructor && (r = t.constructor.name), "Map" === r || "Set" === r ? Array.from(t) : "Arguments" === r || /^(?:Ui|I)nt(?:8|16|32)(?:Clamped)?Array$/.test(r) ? n(t, e) : void 0;
+    //         }
+    //     }
+    //     function n(t, e) {
+    //         (null == e || e > t.length) && (e = t.length);
+    //         for (var r = 0, n = new Array(e); r < e; r++) n[r] = t[r];
+    //         return n;
+    //     }
+    //     document.createElement = function () {
+    //         for (var t = arguments.length, e = new Array(t), r = 0; r < t; r++) e[r] = arguments[r];
+    //         if ("script" !== e[0].toLowerCase()) return i.bind(document).apply(void 0, e);
+    //         var n = i.bind(document).apply(void 0, e);
+    //         try {
+    //             Object.defineProperties(n, {
+    //                 src: {
+    //                     get: function () {
+    //                         return a.src.get.call(this);
+    //                     },
+    //                     set: function (t) {
+    //                         o(t, n.type) && a.type.set.call(this, c), a.src.set.call(this, t);
+    //                     },
+    //                 },
+    //                 type: {
+    //                     set: function (t) {
+    //                         var e = o(n.src, n.type) ? c : t;
+    //                         a.type.set.call(this, e);
+    //                     },
+    //                 },
+    //             }),
+    //                 (n.setAttribute = function (t, e) {
+    //                     "type" === t || "src" === t ? (n[t] = e) : HTMLScriptElement.prototype.setAttribute.call(n, t, e);
+    //                 });
+    //         } catch (t) {
+    //             console.warn("Yett: unable to prevent script execution for script src ", n.src, ".\n", 'A likely cause would be because you are using a third-party browser extension that monkey patches the "document.createElement" function.');
+    //         }
+    //         return n;
+    //     };
+    //     var d = new RegExp("[|\\{}()[\\]^$+*?.]", "g");
+    //     (t.unblock = function () {
+    //         for (var t = arguments.length, r = new Array(t), e = 0; e < t; e++) r[e] = arguments[e];
+    //         r.length < 1
+    //             ? ((s.blacklist = []), (s.whitelist = []))
+    //             : (s.blacklist &&
+    //                   (s.blacklist = s.blacklist.filter(function (e) {
+    //                       return r.every(function (t) {
+    //                           return "string" == typeof t ? !e.test(t) : t instanceof RegExp ? e.toString() !== t.toString() : void 0;
+    //                       });
+    //                   })),
+    //               s.whitelist &&
+    //                   (s.whitelist = [].concat(
+    //                       y(s.whitelist),
+    //                       y(
+    //                           r
+    //                               .map(function (e) {
+    //                                   if ("string" == typeof e) {
+    //                                       var r = ".*" + e.replace(d, "\\$&") + ".*";
+    //                                       if (
+    //                                           s.whitelist.every(function (t) {
+    //                                               return t.toString() !== r.toString();
+    //                                           })
+    //                                       )
+    //                                           return new RegExp(r);
+    //                                   } else if (
+    //                                       e instanceof RegExp &&
+    //                                       s.whitelist.every(function (t) {
+    //                                           return t.toString() !== e.toString();
+    //                                       })
+    //                                   )
+    //                                       return e;
+    //                                   return null;
+    //                               })
+    //                               .filter(Boolean)
+    //                       )
+    //                   )));
+    //         for (var n = document.querySelectorAll('script[type="'.concat(c, '"]')), i = 0; i < n.length; i++) {
+    //             var o = n[i];
+    //             l(o) && (u.blacklisted.push([o, "application/javascript"]), o.parentElement.removeChild(o));
+    //         }
+    //         var a = 0;
+    //         y(u.blacklisted).forEach(function (t, e) {
+    //             var r = p(t, 2),
+    //                 n = r[0],
+    //                 i = r[1];
+    //             if (l(n)) {
+    //                 var o = document.createElement("script");
+    //                 for (var c in (o.setAttribute("src", n.src), o.setAttribute("type", i || "application/javascript"), n)) c.startsWith("on") && (o[c] = n[c]);
+    //                 document.head.appendChild(o), u.blacklisted.splice(e - a, 1), a++;
+    //             }
+    //         }),
+    //             s.blacklist && s.blacklist.length < 1 && f.disconnect();
+    //     }),
+    //         Object.defineProperty(t, "__esModule", { value: !0 });
+    // });
+    
+
+    }
 
 
 function CookieFn() {
@@ -5069,6 +5316,22 @@ function CookieFn() {
     };
 };
 
+function getCookieById (name){
+    const COOKIE_PREFIX = "Influence_";    
+    name = name+"="
+    const cookies = document.cookie.split(";")
+
+    for(var i = 0; i < cookies.length; i++) {
+        var cookie = cookies[i]
+       if(cookie.indexOf(COOKIE_PREFIX+name) == 1){
+            name = cookie.split('=')[0].trim().substring(COOKIE_PREFIX.length);
+            value = cookie.split('=')[1];
+
+        return {name: name, key: value}
+     }
+    }
+    return null;
+}
 
 if (typeof module !== "undefined" && module.exports) module.exports = Note;
 Influence = typeof Influence === 'undefined' ? require('../server') : Influence;
@@ -5116,5 +5379,6 @@ Influence = typeof Influence === 'undefined' ? require('../server') : Influence;
             trackingId: params.trackingId
         });
     }
+
 
 })();
---
+++ HTML/first.js
@@ -0,0 +1,26 @@
+
+
+var script = document.getElementsByTagName('script');
+
+
+console.log(script.length);
+
+
+var arr = [];
+for (var i = 0, ref = script.length = script.length; i < ref; i++) {
+ arr[i] = script[i];
+}
+
+console.log(arr, "$$$$$$$$$$$$$$$$$")
+
+
+// script.forEach(function(e){
+//     console.log(e, "##########")
+// })
+
+// script.map(function(element) {
+
+//     console.log(element)
+// })
+
+// console.log(script, "!111111111111")

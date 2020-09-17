ed3ffa689af762f6592d441e5ef7964ed51d3f54
Mandeep <yadavmandeep96@gmail.com>
2020-09-17 14:04:10 +0530

    a

--- HTML/17sept.js
+++ HTML/17sept.js
@@ -3784,7 +3784,7 @@ var Note = function Note(config, containerStyle, iconStyle) {
 
         var bulkNotiifcationContainer = document.createElement('div')
     //     bulkNotiifcationContainer.className = 'notif-card';
-        bulkNotiifcationContainer.style = type == 'identification' ? "display:block" : "display:none";
+        bulkNotiifcationContainer.style = type == 'custom' ? "display:block" : "display:none";
        // bulkNotiifcationContainer.style = containerStyle;
 
        var bulkNotiifcationMainContainer = document.createElement('div')

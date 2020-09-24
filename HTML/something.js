44aaa5c88f9b2fa1725beb75a7bf47115e91a278
Mandeep <yadavmandeep96@gmail.com>
2020-09-24 16:20:39 +0530

    a

--- HTML/something.js
+++ HTML/something.js
@@ -2328,7 +2328,7 @@ async function loopThroughSplittedNotifications(splittedUrls, enableLoopNotifica
                             let numberOfUsers = info.numberOfUsers && key == 'identification' ? info.numberOfUsers : 0;
                             // let totalCount = info.linkData.totalCount && key == 'custom' ? info.linkData.totalCount : 0;
 
-                            console.log(info.linkData && key == 'custom' ? "info.linkData.totalCount ": 0, "************************")
+                            console.log(info.linkData && key == 'custom' ? info.linkData.totalCount : 0, "************************")
                            
                             liveVisitorCount = liveVisitorCount == 0 ? 1 : liveVisitorCount;
                             //if (((key == 'journey' && !userDetails.length) ||

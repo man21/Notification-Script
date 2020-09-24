a10be73521ed5946599386d4314378c5e55c3a3d
Mandeep <yadavmandeep96@gmail.com>
2020-09-24 16:12:27 +0530

    a

--- HTML/something.js
+++ HTML/something.js
@@ -2326,7 +2326,7 @@ async function loopThroughSplittedNotifications(splittedUrls, enableLoopNotifica
                             let userDetails = info.userDetails;
                             let userReviews = info.userReviews;
                             let numberOfUsers = info.numberOfUsers && key == 'identification' ? info.numberOfUsers : 0;
-                            let totalCount = info.linkData.totalCount && key == 'custom' ? info.linkData.totalCount : 0;
+                            // let totalCount = info.linkData.totalCount && key == 'custom' ? info.linkData.totalCount : 0;
 
                             
                             liveVisitorCount = liveVisitorCount == 0 ? 1 : liveVisitorCount;
@@ -2334,7 +2334,7 @@ async function loopThroughSplittedNotifications(splittedUrls, enableLoopNotifica
                             if (((key == 'journey' && !userDetails) ||
                                 (key == 'review' && !userReviews) ||
                                 (key == 'identification' && !numberOfUsers) ||
-                                (key == 'custom' && !totalCount) ||
+                                // (key == 'custom' && !totalCount) ||
                                 (key == 'live' && (!liveVisitorCount || (configuration && Number(configuration.panelStyle.liveVisitorCount) >= liveVisitorCount)))
                             ) || (configuration && !configuration.activity)) {
                                 j = j - 1;
@@ -2380,9 +2380,9 @@ async function loopThroughSplittedNotifications(splittedUrls, enableLoopNotifica
                                     else if (info.numberOfUsers) key = 'identification';
                                     else if (info.userDetails) key = 'journey';
                                     else if (info.userReviews) key = 'review';
-                                    else if(info.linkData.totalCount) key = 'custom';
-                                    // else if(info.configurations[0].configuration.announcementHeaderText) key = 'announcement' 
-                                    else key = 'announcement'
+                                    // else if(info.linkData.totalCount) key = 'custom';
+                                    else if(info.configurations[0].configuration.announcementHeaderText) key = 'announcement' 
+                                    else key = 'custom'
                                     if(isTabVisibility){
                                         return notificationTimeout(u, info, info.rule, key, notificationPath);}
                                 }, (j==1 ? (info.rule.initialDelay) * 1000 : info.rule.delayNotification ? (randomDelayTime * 1000) :(((info.rule.displayTime + info.rule.delayBetween + 3) * (v-1)) * 1000) + (info.rule.initialDelay) * 1000));

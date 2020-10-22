d7ed9009a0df9474b76ad4778fe88e275c81ac87
Mandeep <yadavmandeep96@gmail.com>
2020-10-22 17:24:19 +0530

    a

--- script/script.js
+++ script/script.js
@@ -3009,7 +3009,7 @@ var Note = function Note(config, containerStyle, iconStyle) {
         }
         
         
-        const leftSideCreator = (type, imageSrc) => {
+        const leftSideCreator = (type) => {
 
             // console.log(type+ "-------------------"+ imageSrc, "+++++++++++++++++++++++++++++++++")
             if (type === "live") {
@@ -3024,12 +3024,12 @@ var Note = function Note(config, containerStyle, iconStyle) {
             const imageElement = divCreator("img", `imageStyle ${type + "-imageStyle"}`)
             imageElement.setAttribute(
                 "src",
-                imageSrc
+                "https://cdn.zeplin.io/5de290feb524497c4a9c9959/assets/C77C11F2-0E34-49DE-97CC-10DF6C848B69.png"
             )
 
             leftSideElement.appendChild(imageElement)
 
-            console.log(leftSideElement, "leftSideElement !!!!!!!!!!!!!!!!!!!!!!!!11")
+            // console.log(leftSideElement, "leftSideElement !!!!!!!!!!!!!!!!!!!!!!!!11")
 
             }
         }
@@ -3204,7 +3204,7 @@ var Note = function Note(config, containerStyle, iconStyle) {
 
         // "https://s3.wasabisys.com/influencelogo/logo/click.svg"
         
-        leftSideCreator(ACTIVE_NOTIFICATION_TYPE, "https://cdn.zeplin.io/5de290feb524497c4a9c9959/assets/C77C11F2-0E34-49DE-97CC-10DF6C848B69.png" )
+        leftSideCreator(ACTIVE_NOTIFICATION_TYPE )
         rightSideTextCreator(ACTIVE_NOTIFICATION_TYPE)
         footerCreator(ACTIVE_NOTIFICATION_TYPE)
         

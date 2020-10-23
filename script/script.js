d7a63d679e44da7b7d8a692de00091972770b9e3
Mandeep <yadavmandeep96@gmail.com>
2020-10-23 16:16:25 +0530

    a

--- script/script.js
+++ script/script.js
@@ -3208,7 +3208,7 @@ var Note = function Note(config, containerStyle, iconStyle) {
             secondaryText
             )
 
-            if(configuration && configuration.panelStyle)
+            if(configuration && configuration.panelStyle && configuration.panelStyle.secondaryColor)
                 span2Element.style = `font-family:${configuration.panelStyle.fontFamily}; color: rgb(${configuration.panelStyle.secondaryColor.r},${configuration.panelStyle.secondaryColor.g},${configuration.panelStyle.secondaryColor.b});`
             
             lineElement.appendChild(span1Element)

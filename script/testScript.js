0564870c57167978ace0bbb6f2e345f9c4e7a3ce
Mandeep <yadavmandeep96@gmail.com>
2020-11-04 12:59:24 +0530

    a

--- script/testScript.js
+++ script/testScript.js
@@ -1,7 +1,7 @@
 var isTabVisibility = true,flagMouseOver= false;
 var exclued_button_text = 'login, signin, loginnow, memberlogin, accountlogin, post comment';
-var __pathname = window.location
-__pathname = '/'+ __pathname.split('/')[1];
+var __pathname = window.location.pathname
+__pathname = '/'// + __pathname.split('/')[1];
 
 console.log(__pathname,  "+++++++++++++++++")
 var influenceScript = 'testScript.js';
